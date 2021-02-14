39d95adf861e7b880c8a7d3c49644f565aa7f85c
SoKeY <sch-midi@windowslive.com>
2021-02-14 12:24:25 +0100

    Change blog color

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -9,8 +9,8 @@ export default function Template({
   return (
     <div className="blog-post-container">
       <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
+        <h1 className="text-black">{frontmatter.title}</h1>
+        <h2 className="text-black">{frontmatter.date}</h2>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
